2f315dc99ac55832c1b46893195b53ec0f061b27
Sharadhi N <maya@mayas-MacBook-Pro.local>
2023-02-02 21:11:08 +0530

    test deploy run - 1

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/maya/Desktop/atineti_maya/atineti/mandal/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_macos.build/Objects-normal/x86_64/shared_preferences_macos_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/maya/Desktop/atineti_maya/atineti/mandal/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_macos.build/DerivedSources/shared_preferences_macos_vers.c
